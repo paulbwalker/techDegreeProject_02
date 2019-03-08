c79387a96efd6ca41e503a4acd1dd761762191bf
Paul Walker <pw97960@gmail.com>
2019-03-08 10:47:03 -0800

    Added new files

--- js/scripts.js
+++ js/scripts.js
@@ -1,93 +1,131 @@
-// This small function hides the tatal students then using the slice method to show the first 10 students.
-const showPage = ( pageNumber, studentList ) => {
-	// Hide full list of students
-	studentList.hide();
+/******************************************
+Treehouse Techdegree:
+FSJS project 2 - List Filter and Pagination
+By Paul B. Walker
+******************************************/
 
-	// displays the right amount of students per page
-	studentList.slice( (( pageNumber - 1 ) * 10 ), ( pageNumber * 10 ) ).show(); 
+'use strict';
 
-};
-showPage( 1, $('li.student-item') );
+const pageHeader = document.querySelector('div.page-header');
+const studentList = document.querySelectorAll('li');
+const linkDiv = document.querySelector('div.pagination');
+const maxStudents = 10;
 
 
-// This function takes the studentList as argument, and creates the linked page per 10 students
-const appendPageLinks = ( studentList ) => {
-	// determine how many pages for this student list
-	const totalPage = Math.ceil( studentList.length / 10 );
-	// create a page link section
-	$('.page').append(`
-			<div class="pagination">
-				<ul></ul>
-			</div> 
-		`);
+// showPage function gets the first ten students starting with zero
+// adds 9 plus 1 with additional students display block or none
+const showPage = (page, students) => {
+   students = document.querySelectorAll('li.student-item');
+   const firstPage = page * maxStudents;
+   const lastStudents = firstPage + 9;
 
-	// use a "for" loop through the divided totalPage variable.
-	for ( let i = 1; i <= totalPage; i++ ) {
-		// add a page link to the page link section append our new page link section to the site/
-		$('.pagination ul').append(`<li><a href="#"> ${i} </a></li>`);
-	}
+   for (let i = 0; i < studentList.length; i += 1) {
+      if (i >= firstPage && i <= lastStudents) {
+         studentList[i].style.display = 'block';
+      } else {
+         studentList[i].style.display = 'none';
+      }
+   }
+}
 
-	// define what happens when you click a link
-	$('.pagination li a').on('click', function(event) {
-		event.preventDefault();
-		// Use the showPage function to display the page for the link clicked
-		showPage( event.target.text, $('li.student-item') );
-		// remove the old page link section by removeClass "active".
-		$('a').removeClass('active');
-		// mark that link as 'active'.
-		event.target.className = 'active';
-	});
-};
-appendPageLinks( $('li.student-item') );
 
-/* Exceeds Grade: The Search Function        *********************************/
+// The appendPageLinks creates the div, ul and appends the html backticks
+// to create the rest of the html element. Added a class on the li if needed.
+const appendPageLinks = (page, students) => {
+   const divPage = document.querySelector('div.page');
+   const linkDiv = document.createElement('div');
+   const ulLink = document.createElement('ul');
+   const numberOfLinks = Math.ceil(studentList.length / maxStudents);
+   divPage.appendChild(linkDiv);
+   linkDiv.classList.add('pagination');
+   linkDiv.appendChild(ulLink);
 
-const searchList = ( studentList ) => {
-	// first append the search bar and button
-	$('.page-header').append(`
-	<div class="student-search">
-	<input type="text" class="find-student" placeholder="Search for students..." />		
-	<button type="submit">Search</button>
-	</div>
-	`);
+   // Create variable to assign it into the Link UL.
+   let addHTML = '';
 
-	// Obtain the value of the search input
-	$('.student-search input').on('keyup', function() {
-		// Declare a variable for input
-		let $input = $(this).val();
+   for (let i = 0; i < numberOfLinks; i += 1) {
+       addHTML +=
+       `<li class="link-list">
+           <a href="#">${i + 1}</a>
+       </li>`
+   }
 
-		// remove the previous page link section
-		$(studentList).hide();
+   // Append the link LI into the Link UL.
+   ulLink.innerHTML = addHTML;
 
-		// Loop over the student list, and for each student...
-		$(studentList).each(function() {
-			// ...obtain the student's name... and the student's email
-			let $listName = $( $(this), '.student-detail h3').text();
+   // Assign the linkDiv into the linkList to use.
+   const linkList = linkDiv.querySelectorAll('li a');
 
-			// ...if the search value is found inside either email or name...
-			if( $listName.search(new RegExp( $input, 'i')) < 0 ) {
-				// If there's no "matched" students...
-				$(this).hide();
-			} else {
-				// ...add this student to list of "matched" student
-				$(this).show();
-			}
-		});
-/* ...display a "no student's found" message ============================== */
-		// Assign student-list into a jQuery variable
-		let $students = $('.student-list');
+   // This is to highlight the first page to show that is where the user is on.
+   if (linkList[0]) { linkList[0].classList.add('active'); }
 
-		// If there's no "matched" students...
-		if( $($students).children(':visible').length < 1 ) {
-			// append the message to the ".student-list"
-			$($students).append(`
-				<span class="alert">No student's found</span>
-				`).css({color: 'red', textAlign: 'center'});
-		} else {
-			// if input matches the ".student-list" remove span
-			$('.alert').remove();
-		}
+   linkDiv.addEventListener('click', (event) => {
+      const pageLinks = linkDiv.querySelectorAll('a'); 
+      for (let i = 0; i < linkList.length; i += 1) {
 
-	});
-};
-searchList( $('li.student-item') );
+         // used a ternary operator to decide to add or remove the active class.
+         (event.target === pageLinks[i]) ? linkList[i].classList.add('active') 
+                                         : linkList[i].classList.remove('active');
+      }
+      // Binds the student list with the pageLinks by finding their index.
+      showPage(Array.from(pageLinks).indexOf(event.target), students, page);
+   });
+}
+
+// The createForm function creates the input and button within the form field
+const studentSearch = () => {
+   const pageHeader = document.querySelector('div.page-header');
+   const searchDiv = document.createElement('div');
+   searchDiv.classList.add("student-search");
+   pageHeader.appendChild(searchDiv);
+   const formField = `<input id="filter-input" type="text" placeholder="Search for students...">
+                      <button>Search</button>`;
+
+   // Append formField into the form element;
+   searchDiv.innerHTML = formField;
+
+   // create local variables. 
+   const ul = document.querySelector('ul.student-list');
+   const input = document.getElementById('filter-input');
+   const button = document.querySelector('button');
+
+   // This function filter through the li element grabs the h3 content and
+   // filter the students name by their LI.
+   const filterNames = () => {
+      const filterValue = input.value.toLowerCase();
+      let li = ul.querySelectorAll('li.student-item');
+      const studentArray = [];
+
+      for (let i = 0; i < li.length; i += 1) {
+         let h3 = li[i].getElementsByTagName('h3')[0];
+
+         if (h3.innerHTML.toLowerCase().indexOf(filterValue) > -1) {
+            li[i].style.display = '';
+            studentArray.push(li[i]);
+         } else {
+            li[i].style.display = 'none';
+         }
+      }
+
+      // The No Student Match Message goes into the UL. If I created a seperate Div
+      // I would have issues of removing the message 
+      if (studentArray.length === 0) {
+         ul.innerHTML = `<li>Sorry, No Students Match Your Search! Please reload browser to get the list.</li>`;
+      } 
+   } 
+
+   // Event Listener for the Search Input.
+   input.addEventListener('keyup', filterNames);
+
+   // When you click the button your remove the text input and No Match message.
+   button.addEventListener('click', () => {
+      ul.innerHTML = '';
+      return input.value = '';
+   });
+
+}
+
+// Called the first page by passing zero to the showPage argument.
+// Calls all the functions for this project.
+showPage(0); appendPageLinks(); studentSearch();
+
